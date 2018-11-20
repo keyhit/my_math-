b5dc9d1b82d9e190672c6669ffb83028af9cfa12
keyhit <viacheslavkarpenkoju@gmail.com>
2018-11-20 23:41:13 +0200

    Refactored Tasks controller, added hidden field task_1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,21 +6,32 @@ class TasksController < ApplicationController
 
   def task_1
     new
+    @task = '1.3'
   end
 
   def task_1_create
-    task = '1.3'
-    # puts "----| Execution 1 | of task #{task} -----".white.on_black
-
+    puts "----| Execution 1 | of task #{@task} -----".white.on_black
     volume = KeyhitPain::Classes::Cube.volume(params_input)
     square = KeyhitPain::Classes::Cube.cube_plane_square(params_input)
-    number_task(task)
     params_results("Volume of cube = #{volume} cubic sm. It's plane square = #{square} square units")
     create
   end
 
   def task_2
     new
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:tasks).permit(:number_task, :condition, :input, :result)
   end
 
   def new
@@ -32,22 +43,6 @@ class TasksController < ApplicationController
     @task.save
 
     redirect_to tasks_path
-  end
-
-  def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-
-    redirect_to tasks_path
-  end
-
-  private
-  def task_params
-    params.require(:tasks).permit(:number_task, :condition, :input, :result)
-  end
-
-  def number_task(arg)
-    params['tasks']['number_task'] = arg
   end
 
   def params_input
